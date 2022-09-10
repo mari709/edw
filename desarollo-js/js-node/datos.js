3cb2c05d591b918324d477293e6f88f6ce832aa0
Marina Mansueto <mansumdp@gmail.com>
2022-09-10 16:49:41 -0300

    datos

---
+++ desarollo-js/js-node/datos.js
@@ -0,0 +1,25 @@
+//Tipos primitivos
+
+//number
+4
+0
+
+//string
+"hola";
+'hola';
+`hola`;
+
+//booleanos
+true;
+false;
+
+//null y undefined
+null; /*espacio definido pero vacio*/
+undefined;
+
+// null, undefined, false, 0 -->falsy
+if(null) {
+    console.log("cumple");
+}else {
+    console.log("no cumple");
+}
